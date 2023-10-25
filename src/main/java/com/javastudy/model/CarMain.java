5b3afa253a79c294a81dabe6286ebd555caefa32
Leonardo Narciso <LNarcisoOV@gmail.com>
2023-10-25 10:35:25 -0300

    Add switch pattern matching.

--- src/main/java/com/javastudy/model/CarMain.java
+++ src/main/java/com/javastudy/model/CarMain.java
@@ -9,8 +9,24 @@ public class CarMain {
         System.out.println(car.toString());
         System.out.println(car.getSizeArea());
         
+        instanceOfPatternMatch(car);
+        switchPatternMatch(car);
+    }
+
+    private static void instanceOfPatternMatch(Car car) {
         if(car.name() instanceof String name && !name.isEmpty()) {
             System.out.println(car.name());
         }
+    }
+
+    private static void switchPatternMatch(Object object) {
+        String result = switch(object) {
+            case null -> "Null value received";
+            case Integer i -> "Integer value received: " + i;
+            case String s -> "String value received: " + s;
+            default -> "None of the types";
+        };
+        
+        System.out.println("Switch pattern match: " + result);
     }
 }
