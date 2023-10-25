75c96dfeb5a47768a04dfd3b143766ace925b597
Leonardo Narciso <LNarcisoOV@gmail.com>
2023-10-25 09:07:35 -0300

    First commit.

---
+++ src/main/java/com/javastudy/Java14151617StudyApplication.java
@@ -0,0 +1,13 @@
+package com.javastudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java14151617StudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java14151617StudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javastudy/Java14151617StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javastudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java14151617StudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
