df98725533e20bc910879344d1347ba0934d1971
Leonardo Narciso <LNarcisoOV@gmail.com>
2024-07-09 18:26:30 -0300

    General fixes.

--- src/main/java/com/javastudy/model/CarMain.java
+++ src/main/java/com/javastudy/model/CarMain.java
@@ -1,5 +1,7 @@
 package com.javastudy.model;
 
+import org.apache.logging.log4j.util.Strings;
+
 public class CarMain {
 
     public static void main(String...strings) {
@@ -14,19 +16,20 @@ public class CarMain {
     }
 
     private static void instanceOfPatternMatch(Car car) {
-        if(car.name() instanceof String name && !name.isEmpty()) {
-            System.out.println(car.name());
+        if (car.name() != null && car.name() instanceof String) {
+            if (!Strings.isBlank(car.name())) {
+                System.out.println(car.name());
+            }
         }
     }
 
-    private static void switchPatternMatch(Object object) {
-//        String result = switch(object) {
-//            case null -> "Null value received";
-//            case Integer i -> "Integer value received: " + i;
-//            case String s -> "String value received: " + s;
-//            default -> "None of the types";
-//        };
-//        
-//        System.out.println("Switch pattern match: " + result);
+    private static void switchPatternMatch (Object object){
+        //String result = switch (object) {
+          //  case null -> "Null value received";
+            //case Integer i -> "Integer value received: " + i;
+            //case String s -> "String value received: " + s;
+        //    default -> "None of the types";
+        //};
+        //System.out.println("Switch pattern match: " + result);
     }
 }
